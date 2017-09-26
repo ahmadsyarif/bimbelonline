94dd08589d5f43630d4750552e9f49191390ab4d
Ahmad Syarif <ahmadsyarif92@gmail.com>
2017-09-26 21:28:53 +0800

    [node] set apiv1 listener

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.get('/apiv1',function(req,res){
+    console.log(req.query);
+});
+
 
 app.listen(3000,function(){
     console.log("running server at port 3000");
-})
+});
+
+
