db9d885f673997378c472fa43eda853a4f53f206
Ahmad Syarif <ahmadsyarif92@gmail.com>
2017-09-26 22:49:19 +0800

    rename file to english

---
+++ server/simulation.js
@@ -0,0 +1,13 @@
+var simulation = (function(){
+
+    var getQuestion = function(request){
+        
+    }
+
+    return {
+        getQuestion:getQuestion
+    }
+
+})();
+
+module.exports = simulation;
