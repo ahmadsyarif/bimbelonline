7552cb5308578c81e6c6bc39c0e23d89232b2e03
Ahmad Syarif <ahmadsyarif92@gmail.com>
2017-09-26 22:53:09 +0800

    [node/html] process simulation in separate javascript file

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const express = require('express');
+const simulation = require('./server/simulation')
 const app = express();
 
 app.use(express.static('public'));
 app.get('/apiv1',function(req,res){
-    var question = {
-        'question_num':'1',
-        'question_des':'Apakah anda merasa sulit meminta tolong pada seorang teman untuk melakukan sesuatu untuk anda ?',
-        'question_op': {
-            'A':'Saya merasa sulit karena saya tidak ingin memberatkan orang lain atas tugas saya.',
-            'B':'Jika dalam kondisi benar – benar mendesak maka saya akan minta tolong orang lain.',
-            'C':'Tergantung situasi dan urgensi, terkadang saya meminta tolong terkadang tidak jika tidak dibutuhkan.',
-            'D':'Saya jarang meminta tolong karena saya merasa itu tugas saya dan saya berusaha menyelesaikannya sendiri.',
-            'E':'Saya tidak pernah merasa sulit karena menurut saya meminta tolong adalah hal yang sopan dan baik.'
-        }
-    }
-    res.send(question);
+    var responseData = simulation.getQuestion(req.query);
+    res.send(responseData);
     console.log(req.query);
 });
 
--- public/js/simulation.js
+++ public/js/simulation.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $.get("http://localhost:3000/apiv1?func=get_question&module_name=cpns&module_num=1&que_num=1",
+    $.get("http://localhost:3000/apiv1?mode=simulation&func=get_question&module_name=cpns&module_num=1&que_num=1",
         function (data) {
             console.log(data);
             updateQuestion(data);   
