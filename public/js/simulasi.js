b764cd471f839b7434d1735280d6c70376e6028d
Ahmad Syarif <ahmadsyarif92@gmail.com>
2017-09-26 21:51:57 +0800

    [html] get the question data from server

--- public/js/simulasi.js
+++ public/js/simulasi.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
     $.get("http://localhost:3000/apiv1?func=get_question&module_name=cpns&module_num=1&que_num=1",
         function (data) {
-            console.log(data);   
+            console.log(data);
+            updateQuestion(data);   
         }
     );
 });
+
+function updateQuestion(data){
+    $("#question_num").text(data.question_num);
+    $("#question_des").text(data.question_des);
+    $("#question_op_a").text(data.question_op.A);
+    $("#question_op_b").text(data.question_op.B);
+    $("#question_op_c").text(data.question_op.C);
+    $("#question_op_d").text(data.question_op.D);
+    $("#question_op_e").text(data.question_op.E);
+
+}
